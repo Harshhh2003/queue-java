905a79bbb19aba7456a09b20b39f44b68ca90fc2
Harshhh2003 <133561905+Harshhh2003@users.noreply.github.com>
2025-02-21 22:25:02 +0530

    feat: Add additional queue operations (offer, poll, peek)

--- quenebaisc.java
+++ quenebaisc.java
@@ -1,16 +1,23 @@
 import java.util.*;
-public class quenebaisc{
+public class quenebaisc {
     public static void main(String[] args) {
         Queue<Integer> q = new LinkedList<>();
+  System.out.println(q.isEmpty());
+  q.add(10);
+        q.add(20);
+        q.add(50);
+        q.add(70);
+        q.add(100);
+        System.out.println(q);
+ q.remove(); 
+        System.out.println(q);
+    System.out.println(q.element());
+        System.out.println(q.peek());
+  q.offer(200);
+      System.out.println(q);
+  q.poll();  
+    System.out.println(q);
+   System.out.println(q.size());
         System.out.println(q.isEmpty());
-    q.add(10);
-    q.add(20);
-    q.add(50);
-    q.add(70);
-    q.add(100);
-    System.out.println(q);
-    q.remove(); // remove the 1st element  =10
-    System.out.println(q);
-    System.out.println(q.element()); // top element 20
     }
 }
