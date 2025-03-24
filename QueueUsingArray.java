14eb8e9eb630d664cccfede42e9bf27597876906
Harshhh2003 <133561905+Harshhh2003@users.noreply.github.com>
2025-03-24 23:01:05 +0530

    quene using array-2

---
+++ QueueUsingArray.java
@@ -0,0 +1,56 @@
+public class QueueUsingArray {
+    public static class Queue {
+        static int arr[];
+        static int size;
+        static int rear = -1;
+
+      Queue(int n) {
+            arr = new int[n];
+            size = n;
+        }
+
+        public static boolean isEmpty() {
+            return rear == -1;
+        }
+
+        public static void add(int data) {
+            if (rear == size - 1) {
+                System.out.println("Queue is full");
+                return;
+            }
+            rear++;
+            arr[rear] = data;
+        }
+
+        public static int remove() {
+            if (isEmpty()) {
+                System.out.println("Queue is empty");
+                return -1;
+            }
+            int front = arr[0];
+            for (int i = 0; i < rear; i++) {
+                arr[i] = arr[i + 1];
+            }
+            rear--;
+            return front;
+        }
+
+        public static int peek() {
+            if (isEmpty()) {
+                System.out.println("Queue is empty");
+                return -1;
+            }
+            return arr[0];
+        }
+    }
+
+    public static void main(String[] args) {
+        Queue q = new Queue(5);
+        q.add(10);
+        q.add(20);
+        q.add(30);
+        
+        System.out.println("Removed: " + q.remove());
+        System.out.println("Front Element: " + q.peek());
+    }
+}
